e5f886fc202a3d5ddb871fa00e8d8e5e328bb167
Sebastian <zeidlerk00@gmail.com>
2026-04-28 19:31:15 -0500

    Added two columns to Dashboard

--- spotify_dashboard.py
+++ spotify_dashboard.py
@@ -3,8 +3,11 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.title("Spotify Explorer")
+# configure page layout to give room for columns
+st.set_page_config(layout="wide")
+st.title("&#127926; Spotify Explorer", text_alignment="left")
 
+# read in dataset
 data = pd.read_csv("data/dataset.csv")
 
 # data cleaning before visualization
@@ -17,31 +20,39 @@ df = df.dropna(subset=['artists', 'album_name', 'track_name'])
 # drop only exact duplicates
 df = df.drop_duplicates()
 
-st.write(f"Loaded **{len(df)}** tracks.")
-st.dataframe(df, use_container_width=True)
+col1, col2 = st.columns(2)
 
-fig = px.bar(
-    df,
-    x="track_genre",
-    y="popularity",
-    color="danceability",
-    hover_data=["track_name", "artists"],
-    height=450,
+with col1:
+    st.write(f"Loaded **{len(df)}** tracks.")
+    st.dataframe(df, use_container_width=True, height=450)
+
+    fig = px.bar( 
+        df,
+        title="Track Genres Ranked by Popularity",
+        x="track_genre",
+        y="popularity",
+        color="danceability",
+        color_continuous_scale=["#ffffff", "#1ED760"],
+        hover_data=["track_name", "artists"],
+        height=450,
+    )
+    st.plotly_chart(fig, use_container_width=True)
+
+with col2:
+    genre = st.selectbox("Genre", sorted(df["track_genre"].unique()))
+    filtered = df[df["track_genre"] == genre]
+
+    st.write(f"**{len(filtered)}** tracks in *{genre}*.")
+    st.dataframe(filtered, use_container_width=True, height=366)
+
+    fig = px.scatter(
+        filtered,
+        title="Popularity vs. Danceability",
+        x="danceability",
+        y="popularity",
+        color="energy",
+        color_continuous_scale=["#ffffff", "#1ED760"],
+        hover_data=["track_name", "artists"],
+        height=450,
 )
-st.plotly_chart(fig, use_container_width=True)
-
-genre = st.selectbox("Genre", sorted(df["track_genre"].unique()))
-filtered = df[df["track_genre"] == genre]
-
-fig = px.scatter(
-    filtered,
-    x="danceability",
-    y="popularity",
-    color="energy",
-    hover_data=["track_name", "artists"],
-    height=450,
-)
-st.plotly_chart(fig, use_container_width=True)
-
-st.write(f"**{len(filtered)}** tracks in *{genre}*.")
-st.dataframe(filtered, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True)
